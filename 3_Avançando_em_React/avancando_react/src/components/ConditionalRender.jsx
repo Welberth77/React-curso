4a891d3501074033cbc1351fb7c3bf1e254cea16
Welberth <welbertholiveira250@gmail.com>
2025-11-07 19:50:04 -0300

    feat: if e else ternário no jsx

--- 3_Avançando_em_React/avancando_react/src/components/ConditionalRender.jsx
+++ 3_Avançando_em_React/avancando_react/src/components/ConditionalRender.jsx
@@ -3,11 +3,24 @@ import { useState } from "react"
 const ConditionalRender = () => {
     const [x] = useState(true)
 
+    const [name] = useState("João")
+
   return (
     <div>
         <h1>Isso será exibido ?</h1>
         {x && <p>Se x for true, sim!</p>}
         {!x && <p>Agora x é false</p>}
+
+        <h1>If e Else ternário </h1>
+        {name === "João" ? (
+          <div>
+            <p>O nome é João</p>
+          </div>
+        ) : (
+          <div>
+            <p>O nome não é João</p>
+          </div>
+        )}
     </div>
   )
 }
