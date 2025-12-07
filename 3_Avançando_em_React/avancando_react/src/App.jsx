9790e7289e8b6193c4c79fbcc3716c1b6fb46bc5
Welberth <welbertholiveira250@gmail.com>
2025-12-07 11:03:09 -0300

    feat: Condicionais em react

--- 3_Avançando_em_React/avancando_react/src/App.jsx
+++ 3_Avançando_em_React/avancando_react/src/App.jsx
@@ -7,10 +7,13 @@ import City from './assets/city.jpg'
 import ManageData from './components/ManageData'
 import ListRender from './components/ListRender'
 import ConditionalRender from './components/ConditionalRender'
+import ShowUserName from './components/ShowUserName'
 
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const[name] = useState("Wel")
 
   return (
     <>
@@ -26,6 +29,7 @@ function App() {
       <ManageData/>
       <ListRender/>
       <ConditionalRender/>
+      <ShowUserName name={name} />
     </>
   )
 }
--- 3_Avançando_em_React/avancando_react/src/components/ConditionalRender.jsx
+++ 3_Avançando_em_React/avancando_react/src/components/ConditionalRender.jsx
@@ -1,16 +1,22 @@
+// Importando o useState
 import { useState } from "react"
 
 const ConditionalRender = () => {
     const [x] = useState(true)
+
+    const[name2] = useState("W elberth")
 
     const [name] = useState("João")
 
   return (
     <div>
         <h1>Isso será exibido ?</h1>
+        {/* (X &&) é igual a verdadeiro */}
         {x && <p>Se x for true, sim!</p>}
+        {/* (!X &&) é igual a falso */}
         {!x && <p>Agora x é false</p>}
 
+        {/* A primeira condição é verdadeira e a outra a falsa */}
         <h1>If e Else ternário </h1>
         {name === "João" ? (
           <div>
@@ -19,6 +25,18 @@ const ConditionalRender = () => {
         ) : (
           <div>
             <p>O nome não é João</p>
+          </div>
+        )}
+
+        <h1>Outro if e else ternário</h1>
+        <h2>Seu nome é Welberth ?</h2>
+        {name2 === "Welberth" ? (
+          <div>
+            <p>Sim meu nome é Welberth</p>
+          </div>
+        ) : (
+          <div>
+            <p>Não, meu nome é {name2}</p>
           </div>
         )}
     </div>
